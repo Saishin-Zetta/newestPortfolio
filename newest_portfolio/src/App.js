ef681ce99ad4dd85fc7b9621212135d218dd9c84
Andrew <andrewy3295@gmail.com>
2023-03-15 23:25:51 -0500

    almost solved

--- newest_portfolio/src/App.js
+++ newest_portfolio/src/App.js
@@ -42,41 +42,8 @@ function App() {
           </Routes>
         </BrowserRouter>
 
-      
+
       </div>
-      <footer className="bg-dark text-center text-white footer">
-
-        <div className="container p-4 pb-0">
-
-          <section className="mb-4">
-
-            <a className="btn btn-outline-light btn-floating m-1" href="https://www.linkedin.com/in/andrewyoung32/" role="button"
-            ><i className="fab fa-facebook-f"></i>LinkedIn</a>
-
-
-            <a className="btn btn-outline-light btn-floating m-1" href="https://github.com/Saishin-Zetta" role="button"
-            ><i className="fab fa-twitter"></i>Github</a>
-
-
-            <a className="btn btn-outline-light btn-floating m-1" href="https://vanderbilt.bootcampcontent.com/andrewy3295-vnKuTb" role="button"
-            ><i className="fab fa-google"></i>Gitlab</a>
-
-
-            <a className="btn btn-outline-light btn-floating m-1" href="https://www.youtube.com/" role="button"
-            ><i className="fab fa-instagram"></i>Youtube</a>
-
-          </section>
-
-        </div>
-
-
-
-        <div className="text-center p-3" >
-          © 2023 Copyright:
-          <a className="text-white" href="https://mdbootstrap.com/"> Andrew's Portfolio</a>
-        </div>
-
-      </footer>
     </>
 
   );
