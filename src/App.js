660dab0417a822296d665170a0de574b37c82862
Andrew <andrewy3295@gmail.com>
2023-03-20 21:29:56 -0500

    it works!

--- src/App.js
+++ src/App.js
@@ -4,47 +4,46 @@ import React from "react";
 
 // import NavBar from './components/NavBar'
 import About from './components/About';
-import Contact from './components/Contact'
-import Blog from './components/Blog'
-import Projects from './components/Projects'
+import Contact from './components/Contact';
+import Blog from './components/Blog';
+import Projects from './components/Projects';
 
 function App() {
   return (
-    <>
-      <header className='navbarHeader'>Andrew Young
-        <ul className="nav justify-content-center">
-          <li className="nav-item">
-            <Link className="nav-link" to='/'>About</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to='/contact'>Contact</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to='/blog'>Blog</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to='/projects'>Project</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to='/resume'>Resume</Link>
-          </li>
-        </ul>
-      </header>
 
-      <div className="App">
-        
-        <HashRouter>
-          <Routes>
-            <Route path='/' element={<About />} />
-            <Route path='/contact' element={<Contact />} />
-            <Route path='/blog' element={<Blog />} />
-            <Route path='/Projects' element={<Projects />} />
-          </Routes>
-        </HashRouter>
+    <div className="App">
+
+      <HashRouter>
+        <header className='navbarHeader'>Andrew Young
+          <ul className="nav justify-content-center">
+            <li className="nav-item">
+              <Link className="nav-link" to='/'>About</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to='/contact'>Contact</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to='/blog'>Blog</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to='/projects'>Project</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to='/resume'>Resume</Link>
+            </li>
+          </ul>
+        </header>
+        <Routes>
+          <Route path='/' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/blog' element={<Blog />} />
+          <Route path='/Projects' element={<Projects />} />
+        </Routes>
+      </HashRouter>
 
 
-      </div>
-    </>
+    </div>
+
 
   );
 }
